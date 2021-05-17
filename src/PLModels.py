94305a9e9e7b151737a6c355347b93a0c2d69a46
youngjae <byeongal@comcom.ai>
2021-05-17 17:23:41 +0900

    Model 코드 잘못 올린 것 수

--- src/PLModels.py
+++ src/PLModels.py
@@ -1,78 +1,99 @@
-import os
-import argparse
-import logging
-
 import torch
-from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint
 
-from PLModels import KoBERT
+from pytorch_lightning.core.lightning import LightningModule
+from torch.utils.data import DataLoader
+from transformers import BertForSequenceClassification, BertTokenizerFast
+from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
+from sklearn.metrics import accuracy_score
 
-
-def get_parser():
-    parser = argparse.ArgumentParser(description='Fine Tune KoBART for Naver Movie Review Sentiment Analysis')
-    parser.add_argument('--train_data_path',
-                        default='./nsmc/ratings_train.txt',
-                        help='train data file path')
-    parser.add_argument('--val_data_path',
-                        default='./nsmc/ratings_test.txt',
-                        help='validation data file path')
-    parser.add_argument('--save_path',
-                        default='./huggingface_model',
-                        help='model and tokenizer path')
-    parser.add_argument('--max_epochs',
-                        default=3,
-                        type=int,
-                        help='')
-    parser.add_argument('--model_path',
-                        default='kykim/bert-kor-base',
-                        help='')
-    parser.add_argument('--batch_size',
-                        default=16,
-                        type=int,
-                        help='how many samples per batch to load')
-    parser.add_argument('--num_workers',
-                        default=4,
-                        type=int,
-                        help='how many subprocesses to use for data loading')
-    parser.add_argument('--lr',
-                        type=float,
-                        default=5e-5,
-                        help='The initial learning rate')
-    parser.add_argument('--warmup_ratio',
-                        type=float,
-                        default=0.1,
-                        help='warmup ratio')
-    return parser
+from Datasets import NSMCDataset
 
 
-def main():
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    parser = get_parser()
-    args = vars(parser.parse_args())
-    logging.info(args)
+class KoBERT(LightningModule):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.save_hyperparameters()
+        self.bert = BertForSequenceClassification.from_pretrained(self.hparams.model_path)
+        self.tokenizer = BertTokenizerFast.from_pretrained(self.hparams.model_path)
 
-    checkpoint_callback = ModelCheckpoint(
-        filename='epoch{epoch}-val_acc{val_acc:.4f}',
-        monitor='val_acc',
-        save_top_k=3,
-        mode='max',
-        auto_insert_metric_name=False,
-    )
+    def forward(self, **kwargs):
+        return self.bert(**kwargs)
 
-    model = KoBERT(**args)
-    trainer = Trainer(
-        callbacks=[checkpoint_callback],
-        max_epochs=args['max_epochs'],
-        num_sanity_val_steps=0,
-        deterministic=torch.cuda.is_available(),
-        gpus=[0] if torch.cuda.is_available() else None,
-    )
-    trainer.fit(model)
-    model.save_hugginface()
+    def step(self, batch, batch_idx):
+        data, labels = batch
+        output = self(input_ids=data, labels=labels)
 
+        loss = output.loss
 
-if __name__ == '__main__':
-    main()
+        y_true = labels.tolist()
+        y_pred = output.logits.argmax(dim=-1).tolist()
+
+        return {
+            'loss': loss,
+            'y_true': y_true,
+            'y_pred': y_pred,
+        }
+
+    def training_step(self, batch, batch_idx):
+        return self.step(batch, batch_idx)
+
+    def validation_step(self, batch, batch_idx):
+        return self.step(batch, batch_idx)
+
+    def epoch_end(self, outputs, state):
+        loss = torch.tensor(0, dtype=torch.float)
+        y_true = []
+        y_pred = []
+        for output in outputs:
+            loss += output['loss'].cpu().detach()
+            y_true.extend(output['y_true'])
+            y_pred.extend(output['y_pred'])
+        loss = loss / len(outputs)
+
+        self.log(state + '_loss', float(loss), on_epoch=True, prog_bar=True)
+        self.log(state + '_acc', accuracy_score(y_true, y_pred), on_epoch=True, prog_bar=True)
+
+    def training_epoch_end(self, outputs):
+        return self.epoch_end(outputs, state='train')
+
+    def validation_epoch_end(self, outputs):
+        return self.epoch_end(outputs, state='val')
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.bert.parameters(), lr=self.hparams.lr)
+
+        num_train_steps = len(self.train_dataloader()) * self.hparams.max_epochs
+        num_warmup_steps = int(num_train_steps * self.hparams.warmup_ratio)
+
+        scheduler = get_cosine_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=num_warmup_steps,
+            num_training_steps=num_train_steps
+        )
+
+        lr_scheduler = {
+            'scheduler': scheduler,
+            'interval': 'step',
+            'frequency': 1
+        }
+
+        return [optimizer], [lr_scheduler]
+
+    def dataloader(self, file_path, shuffle) -> DataLoader:
+        dataset = NSMCDataset(file_path, self.tokenizer)
+        return DataLoader(
+            dataset,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            shuffle=shuffle,
+            pin_memory=True,
+        )
+
+    def train_dataloader(self) -> DataLoader:
+        return self.dataloader(self.hparams.train_data_path, shuffle=True)
+
+    def val_dataloader(self) -> DataLoader:
+        return self.dataloader(self.hparams.val_data_path, shuffle=False)
+
+    def save_hugginface(self):
+        self.bert.save_pretrained(self.hparams.save_path)
